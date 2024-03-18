4f97cabffe217c8e359cd9cc014a483450e08122
Rahulkalsh <108459884+Rahulkalsh@users.noreply.github.com>
2024-03-18 10:00:05 +0530

    Update Benefit.js

--- src/components/Benefit.js
+++ src/components/Benefit.js
@@ -4,21 +4,21 @@ class Benefit extends Component {
     render() {
         return (
             <div>
-                <section class="benefit">
-                    <div class="container">
-                        <h1 class="abtus text-white">Key Benefits</h1>
-                        <div class="row mt-5 d-flex align-items-center">
-                            <div class="col-lg-4 col-md-4 col-12 mt-2 text-center">
-                                <img src="images/benefit1..PNG" alt="feature-img" width="180px" class="img-fluid rounded" />
-                                <h3 class="text-white mt-4">24X7 English Tutor</h3>
+                <section className="benefit">
+                    <div className="container">
+                        <h1 className="abtus text-white">Key Benefits</h1>
+                        <div className="row mt-5 d-flex align-items-center">
+                            <div className="col-lg-4 col-md-4 col-12 mt-2 text-center">
+                                <img src="images/benefit1..PNG" alt="feature-img" width="180px" className="img-fluid rounded" />
+                                <h3 className="text-white mt-4">24X7 English Tutor</h3>
                             </div>
-                            <div class="col-lg-4 col-md-4 col-12 mt-2 text-center">
-                                <img src="images/benefit2.PNG" alt="feature-img" width="180px" class="img-fluid rounded" />
-                                <h3 class="text-white mt-4">Ask Questions Any Time</h3>
+                            <div className="col-lg-4 col-md-4 col-12 mt-2 text-center">
+                                <img src="images/benefit2.PNG" alt="feature-img" width="180px" className="img-fluid rounded" />
+                                <h3 className="text-white mt-4">Ask Questions Any Time</h3>
                             </div>
-                            <div class="col-lg-4 col-md-4 col-12 mt-2 text-center">
-                                <img src="images/benefit3.PNG" alt="feature-img" width="180px" class="img-fluid rounded" />
-                                <h3 class="text-white mt-4">Progress Tracking</h3>
+                            <div className="col-lg-4 col-md-4 col-12 mt-2 text-center">
+                                <img src="images/benefit3.PNG" alt="feature-img" width="180px" className="img-fluid rounded" />
+                                <h3 className="text-white mt-4">Progress Tracking</h3>
                             </div>
                         </div>
                     </div>
